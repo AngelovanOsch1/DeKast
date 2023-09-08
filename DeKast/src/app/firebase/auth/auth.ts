8389ffb5af3d5a3b49926848152141f44750bb69
Angelo <Angelo.van.osch@hotmail.com>
2023-09-08 15:19:23 +0200

    added some more firebase auth functions, added images and fonts, added global font properties

--- DeKast/src/app/firebase/auth/auth.ts
+++ DeKast/src/app/firebase/auth/auth.ts
@@ -4,6 +4,8 @@
 //   signInWithEmailAndPassword,
 //   updatePassword,
 //   reauthenticateWithCredential,
+//   signOut,
+//   onAuthStateChanged,
 // } from 'firebase/auth';
 
 // const auth = getAuth();
@@ -42,14 +44,36 @@
 //     // ...
 //   });
 
-//   const user = auth.currentUser;
+// const user = auth.currentUser;
 
 // // TODO(you): prompt the user to re-provide their sign-in credentials
 // const credential = promptForCredentials();
 
-// reauthenticateWithCredential(user, credential).then(() => {
-//   // User re-authenticated.
-// }).catch((error) => {
-//   // An error ocurred
-//   // ...
-// });
+// reauthenticateWithCredential(user, credential)
+//   .then(() => {
+//     // User re-authenticated.
+//   })
+//   .catch((error) => {
+//     // An error ocurred
+//     // ...
+//   });
+
+// signOut(auth)
+//   .then(() => {
+//     // Sign-out successful.
+//   })
+//   .catch((error) => {
+//     // An error happened.
+//   });
+
+//   onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/auth.user
+//       const uid = user.uid;
+//       // ...
+//     } else {
+//       // User is signed out
+//       // ...
+//     }
+//   });
