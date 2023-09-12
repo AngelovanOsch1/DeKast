a2d3cee40445465d975fd59ceaae55782b323635
kitobirdies <48332379+kitobirdies@users.noreply.github.com>
2023-09-12 14:48:05 +0200

    Added register component + code

---
+++ DeKast/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn   } from '@angular/forms'
+import { ToastService } from '../toast.service';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registrationForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl(''),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    repeatPassword: new FormControl('')
+  });
+
+  constructor(private toastService: ToastService) { }
+
+  ngOnInit() {
+  }
+
+  callingFunction() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+      // Add logic to create an account here
+  
+      // Assuming the account creation failed (for example, due to a database error)
+      this.toastService.show('Oeps, er is iets misgegaan. Probeer het later nog eens');
+    } else {
+      alert('Form is not valid. Please check the fields.');
+    }
+  }
+
+}
